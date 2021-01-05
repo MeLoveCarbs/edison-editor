f7f6739d203ed225dfba9a1e28adb33f5a773b03
anna <gcchenzhen@gmail.com>
2021-01-05 15:48:05 +0800

    add examples

--- examples/src/App.js
+++ examples/src/App.js
@@ -1,28 +1,12 @@
-import React, { useState, createRef, useEffect } from "react";
+import React, { useState, createRef } from "react";
 import { EditorState, RichUtils, getDefaultKeyBinding } from "draft-js";
-import EdisonEditor, { onAddBlock } from "edison-editor";
-import { stateFromHTML } from "draft-js-import-html";
-import { stateToHTML } from "draft-js-export-html";
+import EdisonEditor from "edison-editor";
 import './App.css';
 
 function App() {
   const _draftEditorRef = createRef();
   const [editorState, setEditorState] = useState(EditorState.createEmpty());
-  const [placeholder, setPlaceholder] = useState("请编辑此处");
-  const [isMounted, setMountStatus] = useState(false);
-
-  useEffect(() => {
-    if (!isMounted) {
-      setMountStatus(true);
-      if (window.ReactNativeWebView) {
-        window.ReactNativeWebView.postMessage(
-          JSON.stringify({
-            isMounted: true,
-          })
-        );
-      }
-    }
-  }, [isMounted]);
+  const [placeholder] = useState("请编辑此处");
 
   const handleKeyCommand = (command, editorState) => {
     const newState = RichUtils.handleKeyCommand(editorState, command);
@@ -49,62 +33,6 @@ function App() {
         return getDefaultKeyBinding(e);
     }
   };
-
-  const toggleBlockType = (blockType) => {
-    setEditorState(RichUtils.toggleBlockType(editorState, blockType));
-  };
-
-  const toggleInlineStyle = (inlineStyle) => {
-    setEditorState(RichUtils.toggleInlineStyle(editorState, inlineStyle));
-  };
-
-  const setDefaultValue = (html) => {
-    try {
-      if (html) {
-        setEditorState(EditorState.createWithContent(stateFromHTML(html)));
-      }
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
-  const setEditorPlaceholder = (placeholder) => {
-    setPlaceholder(placeholder);
-  };
-
-  const onAddBlockEntity = (paramsStr) => {
-    try {
-      const { type, params } = JSON.parse(paramsStr);
-      const newState = onAddBlock(type, params, editorState);
-      setEditorState(newState);
-    } catch (err) {
-      console.log(err.message);
-    }
-  };
-
-  const focusTextEditor = () => {
-    _draftEditorRef.current && _draftEditorRef.current.focus();
-  };
-
-  const blurTextEditor = () => {
-    _draftEditorRef.current && _draftEditorRef.current.blur();
-  };
-
-  window.toggleBlockType = toggleBlockType;
-  window.toggleInlineStyle = toggleInlineStyle;
-  window.setDefaultValue = setDefaultValue;
-  window.setEditorPlaceholder = setEditorPlaceholder;
-  window.onAddBlock = onAddBlockEntity;
-  window.focusTextEditor = focusTextEditor;
-  window.blurTextEditor = blurTextEditor;
-
-  if (window.ReactNativeWebView) {
-    window.ReactNativeWebView.postMessage(
-      JSON.stringify({
-        editorState: stateToHTML(editorState.getCurrentContent()),
-      })
-    );
-  }
 
   return (
     <>
